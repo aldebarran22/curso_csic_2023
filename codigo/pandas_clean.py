f9c3e4cb8f961353c88a97d1af2dee5c8c6b1f52
aldebarran22 <56862985+aldebarran22@users.noreply.github.com>
2023-11-17 12:43:31 +0100

    vie33

---
+++ codigo/pandas_clean.py
@@ -0,0 +1,46 @@
+"""
+Limpieza de datos y cambios de tipo en un Dataframe de pandas
+Estudiar la correlación de dos variables: presión vs velocidad
+"""
+import pandas as pd
+import numpy as np
+
+path = "../curso_csic_2023-main/practicas/pandas/csv/"
+
+def correlacion(df):
+    # Estudiar la correlación entre el viento y la presión
+    # np.corrcoef
+    tabla = np.corrcoef(df.Wind, df.Pressure)
+    return round(tabla[0][1] * 100, 4)
+
+def limpiarDF(fichero):
+    df = pd.read_csv(path+fichero, sep=";")
+    df.columns = [col.strip() for col in df.columns]
+    df['Wind'] = pd.to_numeric(df.Wind.str.replace(' mph',''), \
+                               downcast='unsigned')
+    df['Pressure'] = pd.to_numeric(df.Pressure.str.replace(' mb',''), \
+                               downcast='integer')
+    df['Lat'] = pd.to_numeric(df.Lat.str[:-1], downcast='float')
+    df['Lon'] = pd.to_numeric(df.Lon.str[:-1], downcast='float')   
+    """  infer_datetime_format Deprecated!
+    df['DateTime'] = pd.to_datetime(
+        " 2005 " + df.Date + " " + df.Time.str.replace(" GMT",""),
+        infer_datetime_format=True
+    )"""
+    df['DateTime'] = pd.to_datetime(
+        "2005 " + df.Date + " " + df.Time.str.replace(" GMT",""),
+        format="%Y %b %d %H:%M"
+    )
+    df.drop(columns=['Date','Time'], inplace=True)
+    #print(df.info())
+    return df
+
+if __name__=='__main__':
+    df = limpiarDF('IRMA.csv') 
+    df.to_csv("./irma.csv", sep=';', index=False)
+    df = limpiarDF('MARIA.csv') 
+    df.to_csv("./maria.csv", sep=';', index=False)
+    
+    #print(df.head(3))   
+    #print('Correlación W y P: ', correlacion(df),'%')
+
